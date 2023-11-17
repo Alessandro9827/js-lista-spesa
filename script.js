541220788e772028245a87bca410f3b0e5c4d5d7
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-17 19:08:15 +0100

    lista della spesa

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) 
 //gli elementi della lista individualmente con un ciclo while.
+
+const listaSpesa = ["Pasta", "Sugo", "Latte", "Caffè", "Fazzoletti", "Detersivo"];
+
+const output = document.getElementById("output");
+
+for (let i=0; i < listaSpesa.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = listaSpesa[i];
+    output.appendChild(li);
+}
